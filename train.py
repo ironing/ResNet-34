76ea094e26a36946d44211835e19e85e2aa7beba
YJ <susuki8876@163.com>
2019-05-23 15:06:19 +0800

    Update train.py

--- train.py
+++ train.py
@@ -39,10 +39,10 @@ transforms_test = transforms.Compose([
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
-trainset = torchvision.datasets.CIFAR10(root='./data', train=False, download=False, transforms=transforms_train)
+trainset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transforms_train)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=BATCH_SIZE, shuffle=True, num_workers=2)
 
-testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=False, transforms=transforms_test)
+testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transforms_test)
 testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
 # Cifar10的label
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
