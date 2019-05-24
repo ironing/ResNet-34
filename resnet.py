1be5cfbe7c12e8776a5f82d996f2eda7eb62cb3a
YJ <susuki8876@163.com>
2019-05-24 10:35:19 +0800

    Update resnet.py

--- resnet.py
+++ resnet.py
@@ -5,6 +5,7 @@ Author 'Yao-jia'.
 '''
 
 import torch.nn as nn
+import torch.nn.functional as F
 import math
 
 
@@ -91,17 +92,15 @@ class ResNet(nn.Module):
     def __init__(self, block, layers, num_classes=1000):
         self.inplanes = 64
         super(ResNet, self).__init__()
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
-                               bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=1, stride=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(16)
         self.relu = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=1, padding=1)
-        self.layer1 = self._make_layer(block, 64, layers[0])
-        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
-        self.avgpool = nn.AvgPool2d(2, stride=1)
-        self.fc = nn.Linear(512 * block.expansion, num_classes)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 16, layers[0])
+        self.layer2 = self._make_layer(block, 32, layers[1], stride=1)
+        self.layer3 = self._make_layer(block, 64, layers[2], stride=1)
+        self.layer4 = self._make_layer(block, 64, layers[3], stride=1)
+        self.fc = nn.Linear(256 * block.expansion, num_classes)
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
@@ -138,8 +137,8 @@ class ResNet(nn.Module):
         x = self.layer2(x)
         x = self.layer3(x)
         x = self.layer4(x)
+        x = F.avg_pool2d(x, 7)
 
-        x = self.avgpool(x)
         x = x.view(x.size(0), -1)
         x = self.fc(x)
 
